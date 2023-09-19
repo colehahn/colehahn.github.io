e724450ead5096499d535b55fc4c2b9f6a1b1ca9
colehahn <colehahn2000@gmail.com>
2023-09-19 12:33:05 -0400

    styled theme selector

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,61 +1,89 @@
 import React from "react";
 import styled from "styled-components";
 
+export const themes = [
+  {
+    name: "dark",
+    properties: {
+      "--primary": "rgb(53, 124, 127)",
+      "--background": "#303030",
+      "--background2": "#464646",
+      "--text": "lightgray",
+      "--accent": "rgba(226, 109, 92, 0.5)",
+      "--transparent": "rgba(255, 255, 255, 0.1)",
+      "--transparent-more-opaque": "rgba(255, 255, 255, 0.2)",
+    },
+  },
+  {
+    name: "light",
+    properties: {
+      "--primary": "rgb(53, 124, 127)",
+      "--background": "rgb(204, 215, 197)",
+      "--background2": "lightgray",
+      "--text": "black",
+      "--accent": "rgba(226, 109, 92, 0.5)",
+      "--transparent": "rgba(0, 0, 0, 0.1)",
+      "--transparent-more-opaque": "rgba(0, 0, 0, 0.2)",
+    },
+  },
+  {
+    name: "midnight",
+    properties: {
+      "--primary": "rgb(53, 124, 127)",
+      "--background": "black",
+      "--background2": "black",
+      "--text": "lightgray",
+      "--accent": "rgba(226, 109, 92, 0.5)",
+      "--transparent": "rgba(255, 255, 255, 0.1)",
+      "--transparent-more-opaque": "rgba(255, 255, 255, 0.2)",
+    },
+  },
+];
+
 const ThemeSelector = styled.button`
   width: 200px;
   height: 5em;
   padding: 0;
-  border: none;
+  border: 2px solid var(--text);
+  border-radius: 1em;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   font-family: inherit;
   font-size: inherit;
+  overflow: hidden;
+  position: relative;
+  cursor: pointer;
+
+  transition: scale 300ms;
+  &:hover {
+    scale: 1.3;
+    z-index: 1;
+  }
 `;
 
-export const themes = [
-  {
-    name: "dark",
-    properties: [
-      ["--primary", "rgb(53, 124, 127)"],
-      ["--background", "#303030"],
-      ["--background2", "#464646"],
-      ["--text", "lightgray"],
-      ["--accent", "rgba(226, 109, 92, 0.5)"],
-      ["--transparent", "rgba(255, 255, 255, 0.1)"],
-      ["--transparent-more-opaque", "rgba(255, 255, 255, 0.2)"],
-    ],
-  },
-  {
-    name: "light",
-    properties: [
-      ["--primary", "rgb(53, 124, 127)"],
-      ["--background", "rgb(204, 215, 197)"],
-      ["--background2", "lightgray"],
-      ["--text", "black"],
-      ["--accent", "rgba(226, 109, 92, 0.5)"],
-      ["--transparent", "rgba(0, 0, 0, 0.1)"],
-      ["--transparent-more-opaque", "rgba(0, 0, 0, 0.2)"],
-    ],
-  },
-  {
-    name: "midnight",
-    properties: [
-      ["--primary", "rgb(53, 124, 127)"],
-      ["--background", "black"],
-      ["--background2", "black"],
-      ["--text", "lightgray"],
-      ["--accent", "rgba(226, 109, 92, 0.5)"],
-      ["--transparent", "rgba(255, 255, 255, 0.1)"],
-      ["--transparent-more-opaque", "rgba(255, 255, 255, 0.2)"],
-    ],
-  },
-];
+const ThemeSelectorContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`;
+
+const ThemeLabel = styled.p`
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 100%;
+`;
 
 export const setTheme = (i: number) => {
   localStorage.setItem("themeIndex", "" + i);
-  for (const property of themes[i].properties) {
-    document.documentElement.style.setProperty(property[0], property[1]);
+  for (const property of Object.keys(themes[i].properties)) {
+    document.documentElement.style.setProperty(
+      property,
+      (themes[i].properties as any)[property]
+    );
   }
 };
 
@@ -63,26 +91,21 @@ export const Settings: React.FC = () => {
   return (
     <div>
       <h3>Settings</h3>
-      {themes.map((theme, i) => (
-        <ThemeSelector
-          onClick={() => setTheme(i)}
-          key={i}
-          style={{ backgroundColor: theme.properties[1][1] }}
-        >
-          <div style={{ display: "flex" }}>
-            {theme.properties.map((property, i) => (
-              <div
-                style={{
-                  width: `calc(200px / ${theme.properties.length})`,
-                  height: "1em",
-                  backgroundColor: property[1],
-                }}
-              ></div>
-            ))}
-          </div>
-          <p style={{ textAlign: "center", width: "100%" }}>{theme.name}</p>
-        </ThemeSelector>
-      ))}
+      <h4>Theme:</h4>
+      <ThemeSelectorContainer>
+        {themes.map((theme, i) => (
+          <ThemeSelector
+            onClick={() => setTheme(i)}
+            key={i}
+            style={{
+              background: `linear-gradient(${theme.properties["--primary"]} 0 10%, ${theme.properties["--background2"]} 10%, ${theme.properties["--background"]} 30% 70%, ${theme.properties["--background2"]})`,
+              color: theme.properties["--text"],
+            }}
+          >
+            <ThemeLabel>{theme.name}</ThemeLabel>
+          </ThemeSelector>
+        ))}
+      </ThemeSelectorContainer>
     </div>
   );
 };
